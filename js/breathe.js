d0d5e62b3fd5aadf60d18435bae965921b0884bd
IvarssonAndreas <you@example.com>
2020-11-15 15:30:36 +0100

    Add first verison of site

---
+++ js/breathe.js
@@ -0,0 +1,62 @@
+function breathe(numBreaths = 10) {
+
+    let counter = 0;
+    const container = document.getElementById('breathe-container');
+    const actionText = document.getElementById('breathe-text-action');
+    const breathLeftText = document.getElementById('breathe-text-action--2');
+    breathLeftText.innerText = numBreaths;
+
+
+    const totalTime = 10000;
+    const breatheTime = (totalTime / 5) * 2;
+    const holdTime = totalTime / 5;
+
+    breathAnimation();
+
+
+    function breathAnimation() {
+        breathLeftText.innerText = numBreaths - counter;
+
+        if (counter === numBreaths) {
+            counter = 0;
+        }
+        actionText.innerText = 'Breathe In';
+        container.className = 'breathe-container grow';
+
+
+        setTimeout(() => {
+            actionText.innerText = 'Hold';
+
+            setTimeout(() => {
+                actionText.innerText = 'Breathe Out';
+                container.className = 'breathe-container shrink';
+            }, holdTime);
+
+        }, breatheTime);
+        counter++
+
+
+    }
+
+    setInterval(breathAnimation, totalTime);
+
+}
+
+document.getElementById('breathe').innerHTML = `
+        <div class="breathe-container" id="breathe-container">
+            <div class="circle"></div>
+
+            <p class="breathe-text" id="breathe-text">
+                <span class="breathe-text__action" id="breathe-text-action"></span>
+                <span class="breathe-text__action--2" id="breathe-text-action--2"></span>
+            </p>
+
+            <div class="breathe-pointer-container">
+                <span class="breathe-pointer"></span>
+            </div>
+
+            <div class="gradient-circle"></div>
+            <div id="progress-circle" class="gradient-circle--outer"></div>
+        </div>`
+
+breathe()
